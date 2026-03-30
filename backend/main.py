a790e4600f45c0543d2cf009fe29434fb7c745a5
Takeru0412-debug <takerutsukareta0412@gmail.com>
2026-03-30 17:36:20 +0900

    add vercel cors

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://expense-app-xxxx.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
