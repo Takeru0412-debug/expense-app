9b658f754ea333a607033b8b93aae4f8fe6e4c09
Takeru0412-debug <takerutsukareta0412@gmail.com>
2026-03-31 13:38:44 +0900

    fix backend

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://expense-app-xxxx.vercel.app",],
+                   "https://expense-app-git-main-takeru0412-debugs-projects.vercel.app",
+                   "https://expense-app.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
